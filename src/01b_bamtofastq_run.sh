e9684547a88aa0358549883e0b28471a51b442f3
alextidd <at31@sanger.ac.uk>
2025-01-29 10:46:29 +0000

    convert bams to fastq for basejumper

---
+++ src/01b_bamtofastq_run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# cd /lustre/scratch125/casm/team268im/at31/resolveome ; bsub -q long -M2000 -R 'span[hosts=1] select[mem>2000] rusage[mem=2000]' -J bamtofastq -o log/%J_bamtofastq.out -e log/%J_bamtofastq.err 'bash src/01b_bamtofastq_run.sh'
+
+# dirs
+wd=$(pwd)
+mkdir -p out/bamtofastq/
+
+# generate samplesheet
+cat data/resolveome/DNA/samplesheet_local.csv |
+awk -F',' 'BEGIN {OFS=","; print "mapped,index,sample_id,file_type"} NR>1 {print $5, $5 ".bai", $7, "bam"}' \
+> out/bamtofastq/samplesheet.csv
+
+# run
+(
+  cd out/bamtofastq/
+  nextflow run nf-core/bamtofastq \
+    -profile singularity,sanger \
+    --input samplesheet.csv \
+    --outdir . \
+    -w $wd/work/bamtofastq/ \
+)
